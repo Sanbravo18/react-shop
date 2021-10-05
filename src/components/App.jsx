92ccffd95835df7ae181fffbfbc74227ca2f0f0b
Daniel Bravo <daniel.brv@hotmail.cl>
2021-10-05 12:49:49 -0300

    Se soluciona problema al corregir typo al importar react-dom en el archivo index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-    return (
-        <div>
-            Hola mundo
-        </div>
-    )
+	return (
+		<h1>Hola Mundo!</h1>
+	);
 }
 
-export default App
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"), //dist de distribution, puede ser cualquier nombre
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
   resolve: {
