04db1237b0859148e90d055d41c59916b0d11fa9
Daniel Bravo <daniel.brv@hotmail.cl>
2021-10-06 11:52:56 -0300

    Se agregan las paginas, componentes y containers, luego fueron ordenados y se agregaron los estilos

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const NotFound = () => {
-    return (
-        <div>
-            Not Found, Error 404. 👀
-        </div>
-    )
+	return (
+		<div>
+			No encontrado, error 404.
+		</div>
+	);
 }
 
-export default NotFound
+export default NotFound;
