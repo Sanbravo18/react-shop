74c51760bde9f51cfd70c09e8a0f4e631a46cdce
Daniel Bravo <daniel.brv@hotmail.cl>
2021-10-13 17:41:13 -0300

    Se agrega comando watchFiles en el devserver (recarga automática en el navegador)

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: '/',
+    publicPath: "/",
   },
   mode: "development",
   resolve: {
@@ -47,5 +47,6 @@ module.exports = {
   ],
   devServer: {
     historyApiFallback: true,
+    watchFiles: path.join(__dirname, "./**"),
   },
 };
