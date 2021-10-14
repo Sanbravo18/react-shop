27798882c63407fbdea89857d847cb03b968098a
Daniel Bravo <daniel.brv@hotmail.cl>
2021-10-14 16:48:43 -0300

    Se agrega React Context

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,28 +1,28 @@
-import React, {useState} from 'react';
-import '../styles/ProductItem.scss';
+import React, { useContext } from "react";
+import "../styles/ProductItem.scss";
+import AppContext from "../context/AppContext";
+import addToCartImage from "@icons/bt_add_to_cart.svg";
 
-import addToCart from '@icons/bt_add_to_cart.svg'
+const ProductItem = ({ product }) => {
+  const { addToCart } = useContext(AppContext);
 
-
-const ProductItem = ({product}) => {
-	const [cart, setCart] = useState([]);
-	const handleClick = () => {
-		setCart([]);
-	}
-	return (
-		<div className="ProductItem">
-			<img src={product.images[0]} alt={product.title} />
-			<div className="product-info">
-				<div>
-					<p>${product.price}</p>
-					<p>{product.title}</p>
-				</div>
-				<figure onClick={handleClick}>
-					<img src={addToCart} alt="" />
-				</figure>
-			</div>
-		</div>
-	);
-}
+  const handleClick = (item) => {
+    addToCart(item);
+  };
+  return (
+    <div className="ProductItem">
+      <img src={product.images[0]} alt={product.title} />
+      <div className="product-info">
+        <div>
+          <p>${product.price}</p>
+          <p>{product.title}</p>
+        </div>
+        <figure onClick={() => handleClick(product)}>
+          <img src={addToCartImage} alt="" />
+        </figure>
+      </div>
+    </div>
+  );
+};
 
 export default ProductItem;
