e88e801f78d03eed7897f7b7afbae918b3f5e2dc
Daniel Bravo <daniel.brv@hotmail.cl>
2021-10-14 10:41:56 -0300

    Se agrega Imágenes y alias en Webpack

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
 import ProductList from '../containers/ProductList';
 
 const Home = () => {
 	return (
-		<>
+		<div>
+			<Header/>
 			<ProductList />
-		</>
+		</div>
 	);
 }
 
